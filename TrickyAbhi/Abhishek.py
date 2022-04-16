27bbe97414c9e8280a57d422ad13d17c8e73cd8e
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-16 21:15:06 +0530

    Add files via upload

---
+++ TrickyAbhi/Abhishek.py
@@ -0,0 +1,10 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+admins = {}
+SUPPORT = getenv("SUPPORT", "TrickyAbhii_Op")
